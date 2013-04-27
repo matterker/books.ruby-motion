efde1f713999a6f976a3eeeb2ffe5a8e561b474b
matterker <matterker@matterker.com>
2013-04-26 20:36:03 -0500

    1.3

--- HelloMotion/app/app_delegate.rb
+++ HelloMotion/app/app_delegate.rb
@@ -1,5 +1,15 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    @alert = 
+      UIAlertView.alloc.initWithTitle("Hello",
+        message: "Hello, RubyMotion",
+        delegate: nil,
+        cancelButtonTitle: "OK",
+        otherButtonTitles: nil)
+    @alert.show
+
+    puts "Hello from the console!"
+
     true
   end
 end
