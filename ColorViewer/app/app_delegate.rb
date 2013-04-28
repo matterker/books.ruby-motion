7194d78334d36a2ad30845e8fcd59b5681148aae
matterker <matterker@matterker.com>
2013-04-27 22:18:39 -0500

    3.2

--- ColorViewer/app/app_delegate.rb
+++ ColorViewer/app/app_delegate.rb
@@ -3,7 +3,9 @@ class AppDelegate
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.makeKeyAndVisible
 
-    @window.rootViewController = ColorsController.alloc.initWithNibName(nil, bundle: nil)
+    controller = ColorsController.alloc.initWithNibName(nil, bundle: nil)
+    nav_controller = UINavigationController.alloc.initWithRootViewController(controller)
+    @window.rootViewController = nav_controller
     true
   end
 end
