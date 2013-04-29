dc093013225085088e63c54343ed275b95e822fd
matterker <matterker@matterker.com>
2013-04-28 22:35:44 -0500

    4.1

---
+++ UserProfile/app/app_delegate.rb
@@ -0,0 +1,15 @@
+class AppDelegate
+  def application(application, didFinishLaunchingWithOptions:launchOptions)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+
+    @user = User.new
+    @user.id = "123"
+    @user.name = "Clay"
+    @user.email = "clay@mail.com"
+    @user_controller = UserController.alloc.initWithUser(@user)
+    @nav_controller = UINavigationController.alloc.initWithRootViewController(@user_controller)
+    @window.rootViewController = @nav_controller
+    @window.makeKeyAndVisible
+    true
+  end
+end
