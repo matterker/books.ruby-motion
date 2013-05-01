46881bb419015f8e752f2eb5cd8da04b25022185
matterker <matterker@matterker.com>
2013-05-01 14:17:34 -0500

    5.1

---
+++ TableFun/app/controllers/alphabet_controller.rb
@@ -0,0 +1,26 @@
+class AlphabetController < UIViewController
+  def viewDidLoad
+    super
+    self.title = "Alphabet"
+    @table = UITableView.alloc.initWithFrame(self.view.bounds)
+    @table.autoresizingMask = UIViewAutoresizingFlexibleHeight
+    self.view.addSubview(@table)
+
+    @table.dataSource = self
+
+    @data = ("A".."Z").to_a
+  end
+
+  def tableView(tableView, numberOfRowsInSection: section)
+    @data.count
+  end
+
+  def tableView(tableView, cellForRowAtIndexPath: indexPath)
+    @reuseIdentifier ||= "CELL_IDENTIFIER"
+
+    cell = tableView.dequeueReusableCellWithIdentifier(@reuseIdentifier)
+    cell ||= UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier: @reuseIdentifier)
+    cell.textLabel.text = @data[indexPath.row]
+    cell
+  end
+end
