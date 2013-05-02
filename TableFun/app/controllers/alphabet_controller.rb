34ec6c1c315f67e500adb1a292e2b245c0faf778
matterker <matterker@matterker.com>
2013-05-01 21:46:12 -0500

    5.2

--- TableFun/app/controllers/alphabet_controller.rb
+++ TableFun/app/controllers/alphabet_controller.rb
@@ -7,6 +7,7 @@ class AlphabetController < UIViewController
     self.view.addSubview(@table)
 
     @table.dataSource = self
+    @table.delegate = self
 
     @data = ("A".."Z").to_a
   end
@@ -20,7 +21,24 @@ class AlphabetController < UIViewController
 
     cell = tableView.dequeueReusableCellWithIdentifier(@reuseIdentifier)
     cell ||= UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier: @reuseIdentifier)
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator
     cell.textLabel.text = @data[indexPath.row]
     cell
+  end
+
+  def tableView(tableView, didSelectRowAtIndexPath:indexPath)
+    tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    letter = @data[indexPath.row]
+
+    controller = UIViewController.alloc.initWithNibName(nil, bundle: nil)
+    controller.view.backgroundColor = UIColor.whiteColor
+    controller.title = letter
+    label = UILabel.alloc.initWithFrame(CGRectZero)
+    label.text = letter
+    label.sizeToFit
+    label.center = [controller.view.frame.size.width / 2,
+                    controller.view.frame.size.height / 2]
+    controller.view.addSubview(label)
+    self.navigationController.pushViewController(controller, animated: true)
   end
 end
