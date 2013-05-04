239967f5df5618abb9669d2d2a576e2c5354a268
matterker <matterker@matterker.com>
2013-05-04 10:53:03 -0500

    7.3

--- Colr/app/models/color.rb
+++ Colr/app/models/color.rb
@@ -24,4 +24,17 @@ class Color
 
     @tags = tags
   end
+
+  def self.find(hex, &block)
+    BubbleWrap::HTTP.get("http://www.colr.org/json/color/#{hex}") do |response|
+      result_data = BubbleWrap::JSON.parse(response.body.to_str)
+      color_data = result_data["colors"][0]
+      color = Color.new(color_data)
+      if color.id.to_i == -1
+        block.call(nil)
+      else
+        block.call(color)
+      end
+    end
+  end
 end
